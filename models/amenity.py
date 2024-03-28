d3df5f880eb2f0e678e9560cc793d52882079022
root <root@DESKTOP-ACGA9AB>
2024-03-28 16:13:45 +0000

    fdfd

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Amenity module"""
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """Create a new amenity based on baseModel"""
-
-    name = ""
--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""City module"""
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """Create a new city based on baseModel"""
-    state_id = ""
-    name = ""
